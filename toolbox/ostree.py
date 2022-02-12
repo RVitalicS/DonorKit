e2fda0da497cf5f9f75bdf79743c8c1231a14d0e
RVitalicS <46557916+RVitalicS@users.noreply.github.com>
2022-02-12 19:38:14 +0300

    add comments

--- toolbox/ostree.py
+++ toolbox/ostree.py
@@ -66,6 +66,7 @@ import os
 import re
 
 
+# define names of directories
 SUBDIR_PREVIEWS  = "previews"
 SUBDIR_SOURCES   = "sources"
 SUBDIR_VFX       = "vfx"
@@ -85,38 +86,48 @@ def build ( root,
             modelling = False,
             animation = False,
             surfacing = False ):
+    
+    '''
+        Creates required folders to store different parts of asset
+        depending on its complexity
+    '''
 
+            
+    # create folder to be interpreted as asset root
     if not os.path.exists(root):
         os.mkdir(root)
 
-
+    # create folder to store previews
     if previews:
         previews = os.path.join(root, SUBDIR_PREVIEWS)
         if not os.path.exists(previews):
             os.mkdir(previews)
-
+    
+    # create folder to store working files of maya or another DCC
     if sources:
         sources = os.path.join(root, SUBDIR_SOURCES)
         if not os.path.exists(sources):
             os.mkdir(sources)
 
-
+    #  create folder to hairs, smokes, particles and etc.
     if vfx:
         vfx = os.path.join(root, SUBDIR_VFX)
         if not os.path.exists(vfx):
             os.mkdir(vfx)
 
-
+    # create folder to store mesh data
     if modelling:
         modelling = os.path.join(root, SUBDIR_MODELLING)
         if not os.path.exists(modelling):
             os.mkdir(modelling)
-
+            
+    # create folder to store animation data
     if animation:
         animation = os.path.join(root, SUBDIR_ANIMATION)
         if not os.path.exists(animation):
             os.mkdir(animation)
 
+    # create folder to store shaders and textures
     if surfacing:
         surfacing = os.path.join(root, SUBDIR_SURFACING)
         if not os.path.exists(surfacing):
